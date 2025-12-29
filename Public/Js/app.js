6552bb7769ee0bca0bdaf89ae3f6c2ea2ee12729
Anas-elbourkkadi <elbourkkadianas@gmail.com>
2025-12-29 16:33:25 +0100

    added function check password

--- Public/Js/app.js
+++ Public/Js/app.js
@@ -1,4 +1,3 @@
-.
 class User {
     constructor(name, email, age, password) {
         this.n = name;
@@ -67,38 +66,85 @@ function singUpPassword() {
     }
 }
 
+function Cpassword(p) {
+    let car = ["@", "#", "-", "+", "*", "/"]
+    if (p.length >= 7 && !/\s/.test(p)) {
+        if (p.split('').some(e => { return car.includes(e) })) {
+            alert("your password is good.")
+            let Cpassword = prompt("confirme the password:");
+            if (Cpassword == p) {
+                return p;
+            }
+        }
+        else {
+            alert("your password is not good")
+            singUpPassword();
+        }
+    } else {
+        alert("your password is not good")
+        singUpPassword();
+    }
+}
+
 function check() {
-    let checkEmail = prompt("enter your email:");
-    let i = database.findIndex(e => { e.e == checkEmail })
-    if (i!=0){
-        alert("your email is goood")
-        return i
+    let check = prompt("enter your email:");
+    let i = database.findIndex(e => { e.e == check })
+    if (i != 0) {
+        alert("your email is found")
+        check = prompt("enter the password:")
+        if (check == database[i].p) {
+            alert("your password is right.")
+            return true
+        }
+        else {
+            alert("your password is not right.")
+
+        }
     }
-    else{
-       alert("you email is not right");
+    else {
+        alert("you email is not right");
+        check();
     }
+}
+
+function changPassword() {
+    let email = prompt("enter the email:");
+    let i = database.findIndex(e =>   e.e == email )
+    if (i != -1) {
+        alert("your email is right");
+
+        database[i].p = Cpassword(prompt("enter a password:").trim());
+        alert("change the password is scucces.");
+    }
+}
+let stats = true
+while (stats) {
+    let menu = prompt("choice the own:\n1-do you want to sing up (type sing up).\n2-do you want to (type the log in)\n3- do you want to change the password (type 3)\n4- if you want to exit (type exit): ").toLowerCase().trim();
+    let listMenu = ["sing up", "log in", "3", "exit"]
+
+    if (listMenu.includes(menu)) {
+        alert("good")
+        if (menu == "sing up") {
+            let user = new User(singUpN(), singUpEmail(), singUpAg(), singUpPassword());
+            database.push(user);
+            console.log(database);
+        }
+        else if (menu == "log in") {
+            check();
+        }
+        else if (menu == "3") {
+            changPassword();
+            console.log(database);
+            stats = false;
+        }
+    }
+    else {
+        alert("can you enter the own choice in the menu.")
+    }
+
 }
 
 
-
-let menu = prompt("choice the own:\n1-do you want to sing up (type sing up).\n2-do you want to (type the log in)\n3- do you want to change the password (type 3)\n4- if you want to exit (type exit): ").toLowerCase().trim();
-let listMenu = ["sing up", "log in", "3", "exit"]
-
-if (listMenu.includes(menu)) {
-    alert("good")
-    if (menu == "sing up") {
-        let user = new User(singUpN(), singUpEmail(), singUpAg(), singUpPassword());
-        database.push(user);
-        console.log(database);
-    }
-    else if (menu == "log in") {
-
-
-    }
-}
-else {
-    alert("can you enter the own choice in the menu.")
-}
 
 
 
