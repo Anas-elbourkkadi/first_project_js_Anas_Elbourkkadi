4a58c332ab288fdfdbea207ae8fdc881309f10c5
Anas-elbourkkadi <elbourkkadianas@gmail.com>
2025-12-29 12:32:12 +0100

    added the the input full name and the menu function sing up

---
+++ Public/Js/app.js
@@ -0,0 +1,32 @@
+//  Account Creation and Management:
+//     + Allow the user, via prompts, to choose between signing up, logging in, or changing the password.
+//     + If the user only writes "exit," they exit the current process, and the choice question is asked again.
+//         * If the user chooses to sign up, here are the details they must enter:
+//             # Name (Full):
+//             - Check for leading or trailing spaces.
+//             - The first letter should be capitalized.
+//             - After each space, the first letter should remain capitalized.
+//             - Check that all other characters are in lowercase.
+//             - Do not save the Name if it has less than 5 characters (excluding spaces).
+//             - Do not save the Name if it contains numbers, "@", or similar special characters.
+
+
+function singup() {
+    let fullName = prompt("enter your full name").trim().split(" ").map(e => e.charAt(0).toUpperCase()+e.slice(1).toLowerCase()).join(' ')
+   
+
+}
+
+let menu = prompt("choice the own:\n1-do you want to sing up (type sing up).\n2-do you want to (login type the log in)\n3- do you want to change the password (type 3)\n4- if you want to exit (type exit): ").toLowerCase().trim();
+let listMenu = ["sing up", "log in", "3", "exit"]
+
+if (listMenu.includes(menu)) {
+    alert("good")
+    if (menu == "sing up") {
+        singup();
+    }
+}
+else {
+    alert("can you enter the own choice in the menu.")
+}
+
